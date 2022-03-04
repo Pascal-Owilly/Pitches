199018f74601eab1940eab4454b5b0a643a94a21
Owilly <pasaclouma54@gmail.com>
2022-03-04 22:26:19 +0300

    setting up and initializing app

--- app/templates/__init__.py
+++ app/templates/__init__.py
@@ -1,1 +1,11 @@
+# set up flask app
+from flask import Flask
 
+# Initializing app
+def create_app():
+    app = Flask(__name__) # __name__ reps the name of our app
+    app.config['SECRET_KEY'] = 'abc123' #will encript/secure cookies or session data related to our website
+
+    return app
+
+ 
