700c33ff8bb7788a39fd795caa7737e2ea6f9e7b
Owilly <pasaclouma54@gmail.com>
2022-03-04 22:31:32 +0300

    setting up main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
+from distutils.debug import DEBUG
+from app import create_app # since create_app is inside __init__.py i can import it anywhere
 
+app = create_app()
+
+if __name__=='__main__':
+    app.run(debug=True)
