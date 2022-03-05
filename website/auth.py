11c9e9c65ce0f845066966e6b5d79079cbc816ed
Owilly <pasaclouma54@gmail.com>
2022-03-05 21:36:50 +0300

    print data requested to confirm the inner working of the form

--- website/auth.py
+++ website/auth.py
@@ -1,18 +1,22 @@
-from cgitb import text
-from flask import Blueprint, render_template
+
+from flask import Blueprint, render_template, request
+# request will get the form data
 
 
 auth = Blueprint('auth', __name__)
 
 # defining login, logout, signup
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST']) # With GET nad POST requests we are able to accept get and post requests from this route
 def login():
+   
+    data = request.form
+    print(data)
     return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
 def logout():
     return "<h3>You are loged out"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def signin():
     return render_template("sign_up.html")
