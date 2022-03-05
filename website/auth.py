ae9592444c57c2aa06caaf2a952749df449574a5
Owilly <pasaclouma54@gmail.com>
2022-03-05 21:59:52 +0300

    Validate form

--- website/auth.py
+++ website/auth.py
@@ -7,10 +7,8 @@ auth = Blueprint('auth', __name__)
 
 # defining login, logout, signup
 @auth.route('/login', methods=['GET', 'POST']) # With GET nad POST requests we are able to accept get and post requests from this route
-def login():
-   
-    data = request.form
-    print(data)
+def login():   
+
     return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
@@ -19,4 +17,29 @@ def logout():
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def signin():
+
+    # getting user info and storing in the database for creating their accounts
+    # differentiate between get request and post request
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName.request.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        # Checking if the information is valid
+   
+        if len(email) < 4:
+            pass
+        elif len(firstName) < 2:
+            pass
+        elif password1 != password2:
+            pass
+        elif len(password1) < 7:
+            pass
+
+        # if all the conditions are met we add the user to the database
+
+
+
+
     return render_template("sign_up.html")
