d92cdab3d29b79126ce0a7f7f1c7c09d49cb9da1
Owilly <pasaclouma54@gmail.com>
2022-03-05 16:56:00 +0300

    add login, logout and sign-up to authenticatio file

--- app/main.py
+++
@@ -1,7 +0,0 @@
-from distutils.debug import DEBUG
-from app import create_app # since create_app is inside __init__.py i can import it anywhere
-
-app = create_app()
-
-if __name__=='__main__':
-    app.run(debug=True)
---
+++ website/templates/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__) 
+
+    app.config['SECRET_KEY'] = 'abc123'
+
+    # telling flask we have different appllucation containing blueprint 
+    from website.views import views
+    from website.auth import auth
+
+    # After importing the blueprintd we register them with flask app
+    app.register_blueprint(views, url_prefix = '/')
+    app.register_blueprint(auth, url_prefix = '/')
+    
+
+    return app
+
+    
