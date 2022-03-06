9302642c9287c96b0e4f41b6a93a3a775a3652bb
Owilly <pasaclouma54@gmail.com>
2022-03-06 12:24:50 +0300

    setting up database inside danda init file

--- website/templates/__init__.py
+++ website/templates/__init__.py
@@ -1,4 +1,10 @@
 from flask import Flask
+# Setting up database
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 def create_app():
     app = Flask(__name__, template_folder='../templates') # debugging template not found error
