cd5b63b4a092123917964b031f5ab63e32f43c47
Owilly <pasaclouma54@gmail.com>
2022-03-09 11:24:12 +0300

    modify authentication

--- website/auth.py
+++ website/auth.py
@@ -51,8 +51,8 @@ def sign_up():
             flash('First name must be greater than 1 character.', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
-        elif len(password1) < 7:
-            flash('Password must be at least 7 characters.', category='error')
+        elif len(password1) < 4:
+            flash('Password must be at least 4 characters.', category='error')
         else:
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(
                 password1, method='sha256'))
